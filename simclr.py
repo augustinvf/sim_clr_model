fb2e04c460d55a37b5cc5bfe1a5b3504a5fd1075
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-19 10:10:20 +0200

    maj

--- simclr.py
+++ simclr.py
@@ -97,7 +97,7 @@ def train(args: DictConfig) -> None:
                                           transforms.RandomHorizontalFlip(p=0.5),
                                           get_color_distortion(s=0.5),
                                           transforms.ToTensor()])
-    data_dir = hydra.utils.to_absolute_path("/home/aptikal/chenevoa/projets_environnement_py_torch/sim_clr_model)  # get absolute path of data dir")
+    data_dir = hydra.utils.to_absolute_path("/home/aptikal/chenevoa/projets_environnement_py_torch/sim_clr_model")  # get absolute path of data dir
     train_set = CIFAR10Pair(root=data_dir,
                             train=True,
                             transform=train_transform,
